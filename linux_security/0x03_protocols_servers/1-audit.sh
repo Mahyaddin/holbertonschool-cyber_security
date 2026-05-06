d8496b8529f1c9088972e1b3ba6d2c932d2ab6ce
Mahyaddin <13749@holbertonstudents.com>
2026-05-07 01:07:27 +0400

    Final attempt: 2-line script with tr formatting

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E '^(Include|KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive)' /etc/ssh/sshd_config
+grep -E '^(Include|KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive)' /etc/ssh/sshd_config | tr -s ' '
