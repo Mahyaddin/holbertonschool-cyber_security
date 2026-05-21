99c07d7e1e97715fc122ab45609d6afa831765b0
Mahyaddin <13749@holbertonstudents.com>
2026-05-21 22:35:08 +0400

    Fix Task 1: 3-line decimal to binary encoder using bc and printf

--- network_security/2x00_network_fundamentals/1-dec2bin.sh
+++ network_security/2x00_network_fundamentals/1-dec2bin.sh
@@ -1,17 +1,3 @@
-﻿#!/bin/bash
-
-# Əgər arqument verilməyibsə, xəta ilə çıxış et
-if [ -z "$1" ]; then
-    exit 1
-fi
-
-decimal_number=$1
-binary=""
-
-# 8-bitlik ikilik ədədə çevirmə dövrü
-for ((i=7; i>=0; i--)); do
-    bit=$(( (decimal_number >> i) & 1 ))
-    binary="${binary}${bit}"
-done
-
-echo "$binary"
+#!/bin/bash
+decimal=$1
+printf "%08d\n" $(echo "obase=2;$decimal" | bc)
