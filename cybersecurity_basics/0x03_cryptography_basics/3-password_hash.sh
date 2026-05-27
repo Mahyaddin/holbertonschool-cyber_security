6b9677e37f5e900d2c284a2c2b38e9937c94c495
Mahyaddin <13749@holbertonstudents.com>
2026-05-28 01:37:10 +0400

    Update 3-password_hash.sh

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -n "${1}$(openssl rand -hex 8)" | openssl dgst -sha512 | sed 's/^(stdin)= //g' > 3_hash.txt
+echo -n $1$(openssl rand -hex 8) | openssl dgst -sha512 | sed 's/^(stdin)= //g' > 3_hash.txt
