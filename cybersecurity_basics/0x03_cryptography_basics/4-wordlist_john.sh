caa1bb04aea42a577d8cdde83399fa7e8161550a
Mahyaddin <13749@holbertonstudents.com>
2026-05-28 18:11:27 +0400

    Submit task 4 with pre-defined password

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-sha256 $1 --show | cut -d":" -f2 > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-sha256 "$1" --show | cut -d":" -f2 > 4-password.txt
