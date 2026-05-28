8ba3cc8d4f707851e57d3bab2b8f282b44bc65bb
Mahyaddin <13749@holbertonstudents.com>
2026-05-28 18:18:15 +0400

    Fix file content for task 6

--- cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=raw-sha256 $1 --show | cut -d":" -f2 > 6-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-sha256 $1 --show | cut -d":" -f2 > 6-password.txt
