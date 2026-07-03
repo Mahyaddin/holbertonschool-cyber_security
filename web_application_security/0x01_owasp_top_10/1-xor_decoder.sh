b340808b9c6edb70033a4a39dd34fd0bd21a77c4
Mahyaddin <13749@holbertonstudents.com>
2026-07-03 22:43:55 +0400

    Fix: Pure python bytes XOR to avoid tr byte padding

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -n "$1" | sed 's/^{xor}//' | base64 -d | tr '\000-\377' '[\137*]'
+echo -n "$1" | sed 's/^{xor}//' | base64 -d | python3 -c 'import sys; data=sys.stdin.buffer.read(); print("".join(chr(b ^ 0x5F) for b in data), end="")'
